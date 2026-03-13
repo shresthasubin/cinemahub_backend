bbf1c4b3d7b22a2d87e1d27e3ebd912fc0936ada
shresthasubin <shresthasubin938@gmail.com>
2026-03-13 23:17:29 +0545

    change mysql configuration for deployment

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,21 +4,33 @@ dotenv.config({
     path: './.env'
 })
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-    }
-)
+// host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//         user: process.env.MYSQLUSER,
+//             password: process.env.MYSQLPASSWORD,
+//                 database: process.env.MYSQLDATABASE,
+
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: 'mysql'
+//     }
+// )
+
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: "mysql",
+});
 
 const conenctDB = async () => {
     try {
         await sequelize.authenticate()
-        console.log('Database connected successfully, host: ', sequelize.beforeFindAfterOptions.host)
+        console.log('Database connected successfully')
     } catch (err) {
         console.log('Database connection failed: ', err)
         process.exit(1)
