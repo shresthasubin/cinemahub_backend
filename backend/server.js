f54128e74a23d5423bab3b292c79d5626448731c
Prajwal-k0ira1a <prajwalkoirala05@gmail.com>
2026-03-14 02:46:02 +0545

    fix :server side frontend setup

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://cinemahub-frontend.vercel.app/"],
     credentials: true,
   }),
 );
